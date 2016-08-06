b2b1ee97a61d6d78314111709df262e7e4e51b74
Nathan Tannar <nathantannar4@gmail.com>
2016-08-06 16:11:05 -0700

    update

--- Count on Us/FoodViewController.swift
+++ Count on Us/FoodViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import MapKit
 
 class FoodViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
